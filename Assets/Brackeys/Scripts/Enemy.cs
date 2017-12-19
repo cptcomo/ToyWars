c107de0e8fcd4bb3ffbb93aea13a937359b61937
Kevin Ngo <kevin.ngo0407@gmail.com>
2017-12-19 16:17:26 -0500

    Added an enemy object, which paths from the start to the end of the map using waypoints

---
+++ Assets/Brackeys/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Brackeys {
+    public class Enemy : MonoBehaviour {
+        public float speed = 10f;
+        private Transform target;
+        private int wpIndex = 0;
+        private const double wpTolerance = 0.4f; //How close to waypoint is close enough
+
+        private void Start() {
+            target = Waypoints.wps[0];
+        }
+
+        private void Update() {
+            Vector3 dir = (target.position - this.transform.position).normalized;
+            transform.Translate(dir * speed * Time.deltaTime, Space.World);
+
+            if(Vector3.Distance(transform.position, target.position) <= wpTolerance) {
+                nextWaypoint();
+            }
+        }
+
+        void nextWaypoint() {
+            if(wpIndex >= Waypoints.wps.Length - 1) {
+                Destroy(gameObject);
+                return;
+            }
+            wpIndex++;
+            target = Waypoints.wps[wpIndex];
+        }
+    }
+}
---
+++ Assets/Brackeys/Scripts/Waypoints.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Brackeys {
+    public class Waypoints : MonoBehaviour {
+        public static Transform[] wps;
+
+        private void Awake() {
+            wps = new Transform[transform.childCount];
+            for(int i = 0; i < wps.Length; i++) {
+                wps[i] = transform.GetChild(i);
+            }
+        }
+    }
+}
+
