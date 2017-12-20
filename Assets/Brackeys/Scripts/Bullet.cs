c06988ba0962c3c66dfb1bd0efef61f1b96a6130
Kevin Ngo <kevin.ngo0407@gmail.com>
2017-12-19 20:01:12 -0500

    I added in a turret with shooting functionality as seen in parts 4 and 5 of tutorial

---
+++ Assets/Brackeys/Scripts/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Brackeys {
+    public class Bullet : MonoBehaviour {
+        private Transform target;
+        public float speed = 70f;
+        public GameObject impactEffect;
+
+        public void seek(Transform target) {
+            this.target = target;
+        }
+
+        private void Update() {
+            if(this.target == null) {
+                Destroy(gameObject);
+                return;
+            }
+            Vector3 dir = (target.position - transform.position);
+            float distToMove = speed * Time.deltaTime;
+
+            if(dir.magnitude <= distToMove) {
+                hitTarget();
+                return;
+            }
+
+            transform.Translate(dir.normalized * distToMove, Space.World);
+        }
+
+        void hitTarget() {
+            GameObject effectInstance = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+            Destroy(effectInstance, 2f);
+            Destroy(target.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Brackeys/Scripts/Turret.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Brackeys {
+    public class Turret : MonoBehaviour {   
+        
+        [Header("Attributes")]
+        public float range = 15f;
+        public float fireRate = 1f;
+        private float fireCountdown = 0;
+        public float turnSpeed = 10f;
+
+        [Header("Unity Setup Fields")]
+        public string enemyTag = "Enemy";
+        private Transform target;
+        public Transform partToRotate;
+        public GameObject bulletPrefab;
+        public Transform firePoint;
+
+        private void Start() {
+            InvokeRepeating("updateTarget", 0f, 0.5f);
+        }
+
+        private void Update() {
+            if(target == null)
+                return;
+
+            //Target lock on
+            Vector3 dir = target.position - this.transform.position;
+            Quaternion lookRotation = Quaternion.LookRotation(dir);
+            Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+            partToRotate.rotation = Quaternion.Euler(0, rotation.y, 0);
+
+            if(fireCountdown <= 0f) {
+                shoot();
+                fireCountdown = 1f / fireRate;
+            }
+
+            fireCountdown -= Time.deltaTime;
+        }
+
+        void updateTarget() {
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+            float minDist = Mathf.Infinity;
+            GameObject nearestEnemy = null;
+            foreach(GameObject enemy in enemies) {
+                float dist = Vector3.Distance(transform.position, enemy.transform.position);
+                if(dist < minDist) {
+                    minDist = dist;
+                    nearestEnemy = enemy;
+                }
+            }
+            if(nearestEnemy != null && minDist <= range) {
+                target = nearestEnemy.transform;
+            } else target = null;
+        }
+
+        private void OnDrawGizmosSelected() {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, range);
+        }
+
+        void shoot() {
+            GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Bullet bullet = bulletGO.GetComponent<Bullet>();
+            if(bullet != null) {
+                bullet.seek(target);
+            }
+        }
+    }
+}
