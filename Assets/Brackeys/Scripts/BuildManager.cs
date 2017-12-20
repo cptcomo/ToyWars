2994b594f66308a0e40b3d3304e63cbb95d5c38d
Kevin Ngo <kevin.ngo0407@gmail.com>
2017-12-19 22:34:00 -0500

    Implemented clicking on nodes to build turrets as in part 6

---
+++ Assets/Brackeys/Scripts/BuildManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Brackeys {
+    public class BuildManager : MonoBehaviour {
+        public static BuildManager instance;
+
+        private void Awake() {
+            if(instance != null) {
+                Debug.LogError("More than one build manager in scene!");
+                return;
+            }
+            instance = this;
+        }
+
+        public GameObject standardTurretPrefab;
+
+        private void Start() {
+            turretToBuild = standardTurretPrefab;
+        }
+
+        private GameObject turretToBuild;
+
+        public GameObject getTurretToBuild() {
+            return turretToBuild;
+        }
+    }
+}
---
+++ Assets/Brackeys/Scripts/Node.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Brackeys {
+    public class Node : MonoBehaviour {
+        public Color hoverColor;
+        public Vector3 posOffset;
+
+        private GameObject turret;
+
+        private Renderer rend;
+        private Color startColor;
+
+        private void Start() {
+            rend = GetComponent<Renderer>();
+            startColor = rend.material.color;
+        }
+
+        private void OnMouseEnter() {
+            rend.material.color = hoverColor;
+        }
+
+        private void OnMouseExit() {
+            rend.material.color = startColor;
+        }
+
+        private void OnMouseDown() {
+            if(turret != null) {
+                Debug.Log("Can't build there! - TODO: Display on screen");
+                return;
+            }
+
+            GameObject turretToBuild = BuildManager.instance.getTurretToBuild();
+            turret = (GameObject)Instantiate(turretToBuild, transform.position + posOffset, transform.rotation);
+        }
+    }
+}
