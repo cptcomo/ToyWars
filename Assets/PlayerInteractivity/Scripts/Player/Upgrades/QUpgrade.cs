7c75dbbeecc5e85ee0a45b39e80991c48e6c84e5
cptcomo <Kevin.ngo0407@gmail.com>
2018-01-25 15:43:12 -0500

    I significantly buffed the player's abilities and upgrades

--- Assets/PlayerInteractivity/Scripts/Player/Upgrades/QUpgrade.cs
+++ Assets/PlayerInteractivity/Scripts/Player/Upgrades/QUpgrade.cs
@@ -6,10 +6,12 @@ namespace PlayerInteractivity {
     public class QUpgrade : AbilityUpgrade {
         public float cdReduce;
         public float damageInc;
+        public float rangeInc;
         public override void activate(Player player) {
             Q q = (Q)player.Q;
             q.damage += damageInc;
             q.cooldown -= cdReduce;
+            q.range += rangeInc;
         }
     }
 }
