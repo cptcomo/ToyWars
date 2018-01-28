bd3b2f039cb9e772dc0703255e4389b5f71c6fbf
Kevin Ngo <kevin.ngo0407@gmail.com>
2018-01-28 17:19:02 -0500

    I created the map for the production-version of my game. It has three paths and 2 bases - one for the player and one for the opponent

---
+++ Assets/Toywars/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Toywars {
+    public class MainMenu : MonoBehaviour {
+        [SerializeField]
+        private string levelToLoad;
+
+        private void Start() {
+            Time.timeScale = 1;
+        }
+
+        public void play() {
+            SceneManager.LoadScene(levelToLoad);
+        }
+
+        public void quit() {
+            Application.Quit();
+        }
+    }
+}
