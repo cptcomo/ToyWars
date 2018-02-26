b6b6d40a50a91318d9a5ce4041d6ec3efc7bbd1e
Kevin Ngo <kevin.ngo0407@gmail.com>
2018-02-26 18:33:59 -0500

    I re-added the lives text to show how much the base health is

---
+++ Assets/Toywars/Scripts/Player/LivesUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Toywars {
+    public class LivesUI : MonoBehaviour {
+        public Text livesText;
+
+        PlayerManager pm;
+
+        private void Start() {
+            pm = PlayerManager.getInstance();
+        }
+
+        private void Update() {
+            livesText.text = "Lives: " + pm.baseHealth;
+        }
+    }
+}
+
